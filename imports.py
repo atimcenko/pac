b4cc3df9ad910b43abd48b0edb2c1ba3417706a8
Alexey Timchenko <alesha020@gmail.com>
2022-02-28 13:06:49 +0300

    added imports file for convenience

---
+++ imports.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+
+#!pip install pyEDFlib
+import pyedflib
+#!pip install ipympl
+
+from scipy.fftpack import fft, ifft, fftfreq
+from scipy import signal as sg
+from scipy.ndimage.filters import gaussian_filter1d, gaussian_filter
+from scipy.stats import binned_statistic, entropy, norm
+from statsmodels.stats.multitest import multipletests
+import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression
+
+from fooof import FOOOF
+
+import sys
+import os
+import time
+#import pickle
+import dill as pickle
+
+import concurrent.futures
+from numba import jit, njit, prange
+
+from tqdm.notebook import tqdm
+from collections import defaultdict
+import itertools
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+#from IPython.display import display
+sns.set(context='notebook', style='ticks', palette='bright', font='sans-serif', font_scale=1, color_codes=True, rc=None)
+plt.rcParams['figure.figsize'] = (10, 6)
+
+#print("SYS.PATH: ", sys.path[:3])
+#sys.path.insert(0, r"C:\Users\User\[[Python]]\[AlexeyT]\PAC_PROJECT")
+
+from utility_functions import *
+from lfp_class import LFP
+from pac_class import MyPAC
+from patient_class import Patient
+# %run utility_functions.py
+# %run lfp_class.py
+# %run pac_class.py
+# %run patient_class.py
+
+print("Succesfully imported libraries and modules\n")
